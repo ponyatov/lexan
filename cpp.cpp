f70ad27079a4afe89c17e472e50fd768c077cb50
Dmitry Ponyatov <dponyatov@gmail.com>
2016-02-24 15:54:01 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

---
+++ cpp.cpp
@@ -0,0 +1,38 @@
+#include "hpp.hpp"
+#define YYERR "\n\n"<<yylineno<<":"<<msg<<"["<<yytext<<"]\n\n"
+void yyerror(string msg) { cout<<YYERR; cerr<<YYERR; exit(-1); }
+int main() { glob_init(); yyparse(); 
+	cout<<module.eval()->dump()<<"\n\n"; return 0; }
+
+Sym::Sym(string T,string V) { tag=T; val=V; env=&glob; }
+Sym::Sym(string V):Sym("",V) {}
+void Sym::push(Sym*o) { nest.push_back(o); }
+
+string Sym::tagval() { return "<"+tag+":"+val+">"; }
+string Sym::pad(int n) { string S; for (int i=0;i<n;i++) S+='\t'; return S; }
+string Sym::dump(int depth) {
+	string S = "\n"+pad(depth)+tagval()+env->dump();
+	for (auto it=nest.begin(),e=nest.end();it!=e;it++)
+		S += (*it)->dump(depth+1);
+	return S; }
+
+Sym* Sym::eval() { return this; }
+
+Sym* Sym::eq(Sym*o) { env->set(val,o); return o; }
+
+Op::Op(string V):Sym("op",V) {}
+Sym* Op::eval() {
+	if (val=="=") return nest[0]->eq(nest[1]);
+	return this;
+}
+
+Env::Env(Env*X) { next=X; }
+void Env::set(string V,Sym*o) { iron[V]=o; }
+string Env::dump() { string S;
+	for (auto it=iron.begin(),e=iron.end();it!=e;it++)
+		S += ","+it->first;
+	return S; }
+
+Env glob(NULL);
+void glob_init(){ glob.iron["nil"] = new Sym("nil",""); }
+
---
+++ hpp.hpp
@@ -0,0 +1,42 @@
+#ifndef _H_LEXAN
+#define _H_LEXAN
+
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+#include <map>
+using namespace std;
+
+struct Sym;
+struct Env {
+	map<string,Sym*> iron;
+	Env(Env*); Env* next;
+	Sym* lookup(string);
+	void set(string,Sym*);
+	string dump();
+};
+extern Env glob;
+extern void glob_init();
+struct Sym {
+	string tag,val;
+	Sym(string,string); Sym(string);
+	vector<Sym*> nest; void push(Sym*); void pop();
+	Env* env;
+	string dump(int depth=0); string pad(int);
+	virtual string tagval(); string tagstr();
+	virtual Sym* eval();
+	virtual Sym* eq(Sym*);
+};
+
+struct Op: Sym { Op(string); Sym*eval(); };
+
+extern int yylex();
+extern int yylineno;
+extern char* yytext;
+#define TOC(C,X) { yylval.o = new C(yytext); return X; }
+extern int yyparse();
+extern void yyerror(string);
+extern Sym module;
+#include "ypp.tab.hpp"
+
+#endif // _H_LEXAN
